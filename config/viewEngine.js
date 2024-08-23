6f476b191c8764e7780028e0a45ab37acde6e427
Cristian Florea <floreact@gmail.com>
2024-08-23 13:43:50 +0200

    Updated the viewEngine code

--- config/viewEngine.js
+++ config/viewEngine.js
@@ -1,11 +1,17 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
-// Configuring the view engine
-
+// Configuring the view engine and serving static files
 module.exports = {
-    configViewEngine(app) {
-        app.use(express.static('./public'));
-        app.set('view engine', 'ejs')
-        app.set('views', './views')
-    }
-}
+  configViewEngine(app) {
+    console.log("Path is ", path.join(__dirname, "../views"));
+
+    // Set the view engine to EJS
+    app.set("view engine", "ejs");
+    app.engine("ejs", require("ejs").__express);
+    app.set("views", path.join(__dirname, "../views"));
+
+    // Serve static files from the 'public' directory
+    app.use(express.static(path.join(__dirname, "../public")));
+  },
+};
